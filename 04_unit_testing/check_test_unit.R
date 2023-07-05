67c95d8eb5af09835db9b88a36c450209408f655
Vera Karlbauer <vera@karlbauer.org>
2023-07-05 17:05:34 +0200

    updated for additional functions

--- 04_unit_testing/check_test_unit.R
+++ 04_unit_testing/check_test_unit.R
@@ -9,12 +9,33 @@ load("04_unit_testing/resultsobj_for_testing.Rda")
 # load data
 load("01_data/all_t0.Rda")
 
-# source function
-source("02_analysis/00_functions/setup_resultsobj.R")
+### loop_lm_simple
+
+# source loop_lm_simple function
+source("02_analysis/00_functions/loop_lm_simple.R")
 
 # run function on data
-resultsobj <- setup_resultsobj(data = all_t0)
+resultsobj <- loop_lm_simple(data = all_t0, predictor_name = "Case.Control")
 
-# test whether first column is identical
-diffmeth_malt_t0[, 1] == resultsobj[,1]
+# test whether CpG names are identical
+diffmeth_malt_t0[, 1] == resultsobj[, 1]
 
+# test whether adjusted R squared output is identical
+diffmeth_malt_t0[, 2] == resultsobj[, 2]
+
+# test whether p value output is identical
+diffmeth_malt_t0[, 3] == resultsobj[, 3]
+
+# test whether coefficient output is identical
+diffmeth_malt_t0[, 4] == resultsobj[, 4]
+
+### modify_lm
+
+
+### call_significance_lm
+
+
+### analysis_lm_simple
+
+
+### write_pubtable_lm
