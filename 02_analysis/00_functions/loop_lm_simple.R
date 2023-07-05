5cef0eb7330250da9cbf2017dfb6f6e539d91f49
Vera Karlbauer <vera@karlbauer.org>
2023-07-05 16:47:46 +0200

    Split loop_lm_simple function into parts First part: specify linear model, run linear model, return results

---
+++ 02_analysis/00_functions/loop_lm_simple.R
@@ -0,0 +1,24 @@
+loop_lm_simple <- function(data, predictor_name){
+  # set up results object with CpG names
+  resultsobj <- cbind(colnames(data[1:49]),colnames(data[1:49]),colnames(data[1:49]),colnames(data[1:49]))
+  # loop linear models over all GpGs
+  for(i in 1:49){
+    # linear model with pre-specified covariates
+    # age, sex
+    fit = lm(formula = data[,i] ~ data$age + data$Sex 
+             # genetic components
+             + data$C1 + data$C2 + data$C3 
+             # cell type counts
+             + data$CD14 + data$CD34 + data$Buccal 
+             # prenatal smoke & alcohol exposure
+             + data$smoke_exposure_score + data$alcohol_exposure_score 
+             # predictor of interest
+             + data[, predictor_name], data = environment())
+    # extract summary measures
+    s = summary(fit)
+    resultsobj[i,2] = s$adj.r.squared
+    resultsobj[i,3] = s$coefficients["data[, predictor_name]", "Pr(>|t|)"]
+    resultsobj[i,4] = s$coefficients["data[, predictor_name]", "Estimate"]
+  }
+  return(resultsobj)
+}
