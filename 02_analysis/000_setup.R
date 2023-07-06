7a16bd1e15f39528a77e474d1257346b353b0e0b
Vera Karlbauer <vera@karlbauer.org>
2023-07-06 14:02:54 +0200

    Initial commit

---
+++ 02_analysis/000_setup.R
@@ -0,0 +1,11 @@
+### Setup
+
+# wd
+setwd("/Users/vera_karlbauer/Documents/sib-best-practices-course")
+
+# libraries
+library(rmarkdown)
+library(dplyr)
+
+# import data
+load("01_data/all_t0.rda")
