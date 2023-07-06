38e1cf310d7b1297c37dd1804b1a3d9aef5cb81c
Vera Karlbauer <vera@karlbauer.org>
2023-07-06 20:25:05 +0200

    Updated test script

--- 99_testing/test-analysis_lm_simple.R
+++ 99_testing/test-analysis_lm_simple.R
@@ -1,23 +1,28 @@
-### snapshot testing for loop_lm.simple
-library(testthat)
-local_edition(3)
+## set wd (this is necessary even if you ran the setup script before)
+setwd("/Users/vera_karlbauer/Documents/sib-best-practices-course")
 
-setwd("~/Documents/sib-best-practices-course")
-
-# source original function
+## source functions
+# old
 source("02_analysis/01_functions/00_functions.R")
+# new
+source("02_analysis/01_functions/analysis_lm_simple.R")
 
-# source new function
-# source("02_analysis/01_functions/analysis_lm_simple.R")
+## write function output
+# old testcase 1
+results_old_test_1 <- as.data.frame(loop_lm_old(data = all_t0, predictor_name = "Case.Control"))
 
-# write original snapshot
-test_that("analysis_lm_simple", {
-  expect_snapshot(loop_lm_simple(all_t0, "genotype"))
-  expect_snapshot(loop_lm_simple(all_t2, "Case.Control"))
-})
+# old testcase 2
+results_old_test_2 <- as.data.frame(loop_lm_old(data = all_t2, predictor_name = "genotype"))
 
-# write new snapshot
-# test_that("analysis_lm_simple", {
-#   expect_snapshot(analysis_lm_simple(all_t0, "genotype"))
-#   expect_snapshot(analysis_lm_simple(all_t2, "Case.Control"))
-# })
+# new testcase 1
+results_new_test_1 <- analysis_lm_simple(data = all_t0, predictor_name = "Case.Control")
+
+# new testcase 2
+results_new_test_2 <- analysis_lm_simple(data = all_t2, predictor_name = "genotype")
+
+## compare equality of results
+# testcase 1
+expect_equal(results_new_test_1, results_old_test_1)
+
+# testcase 2
+expect_equal(results_new_test_2, results_old_test_2)
