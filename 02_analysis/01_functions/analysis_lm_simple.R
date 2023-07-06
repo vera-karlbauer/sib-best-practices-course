d4622d778fd2d82cd25409d92b10f51f9b45f9d7
Vera Karlbauer <vera@karlbauer.org>
2023-07-06 20:24:19 +0200

    Corrected assignment of resultsobj

--- 02_analysis/01_functions/analysis_lm_simple.R
+++ 02_analysis/01_functions/analysis_lm_simple.R
@@ -3,9 +3,8 @@ source("02_analysis/01_functions/modify_resultsobj.R")
 source("02_analysis/01_functions/call_significance.R")
 
 analysis_lm_simple <- function(data, predictor_name){
-  loop_lm_simple(data)
-  modify_lm(resultsobj)
-  call_significance_lm(resultsobj)
+  resultsobj <- loop_lm_simple(data, predictor_name)
+  resultsobj <- modify_lm(resultsobj)
+  resultsobj <- call_significance_lm(resultsobj)
   return(resultsobj)
 }
-  
